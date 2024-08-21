53a53ff7c34bf2597b57d7669524a53aae243bd9
Vinnie <95139694+0001-Vinnie@users.noreply.github.com>
2024-08-22 00:35:47 +0300

    Create Rich_Presence_NoButtons.js
    Discord "Rich Presence" no button version

---
+++ Rich_Presence_NoButtons.js
@@ -0,0 +1,22 @@
+const RPC = require('discord-rpc');
+const clientId = '0000'; // Client ID you received from Discord Developer Portal
+
+// Initialize Discord RPC
+const rpc = new RPC.Client({ transport: 'ipc' });
+
+rpc.on('ready', () => {
+    rpc.setActivity({
+        details: ' - ', //Your detail
+        state: ' - ', //Your Status
+        startTimestamp: new Date(),
+        largeImageKey: ' - ', // Your Large image key
+        largeImageText: ' - ', // Your Large image text
+        smallImageKey: ' - ', // Your Small image key
+        smallImageText: ' - ', // Your Small image text
+        
+    });
+
+    console.log('Rich Presence is now active!');
+});
+
+rpc.login({ clientId }).catch(console.error);
